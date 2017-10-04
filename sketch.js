a94e3b5732c8aed79f502a132430d38a4bda80c1
Vivianne Burson <bursonv@oregonstate.edu>
2017-10-04 09:50:08 -0700

    Cirlces move! Cool!!

--- sketch.js
+++ sketch.js
@@ -1,12 +1,18 @@
 // Zombulator by Vivianne Burson
 
+var zombieX = 80;
+var zombie2X = 100;
+
 function setup() {
- createCanvas(800, 800);
+	createCanvas(800,800);
 }
 
- function draw() {
-  fill(255, 0, 0);
-  ellipse(50, 50, 80, 80);
-  fill(150, 150, 200);
-  ellipse(200, 100, 80, 80);
- }
+function draw() {
+	background(255, 255, 255)
+	fill(244, 244, 66);
+	ellipse(zombieX, 50, 80, 80);
+	// fill(150, 150, 200);
+	ellipse(zombie2X, 100, 80, 80); 
+	zombieX = zombieX + 1;
+	zombie2X = zombie2X + 3;
+}
