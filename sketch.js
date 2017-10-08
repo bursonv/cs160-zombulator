7b44d215b0254509e1a6ad89d073804fb46fa603
Vivianne Burson <bursonv@oregonstate.edu>
2017-10-08 15:04:12 -0700

    Made one circle wrap around

--- sketch.js
+++ sketch.js
@@ -2,17 +2,30 @@
 
 var zombieX = 80;
 var zombie2X = 100;
+var edge =1000;
+var speed1 = 1; 
+var size = 80;
 
 function setup() {
-	createCanvas(800,800);
+	createCanvas(edge,800);
+	background(255, 255, 255);
 }
 
 function draw() {
-	background(255, 255, 255)
-	fill(244, 244, 66);
+	strokeWeight(4);
+	fill(random(255), random(255), random(255));
 	ellipse(zombieX, 50, 80, 80);
 	// fill(150, 150, 200);
 	ellipse(zombie2X, 100, 80, 80); 
-	zombieX = zombieX + 1;
-	zombie2X = zombie2X + 3;
-}
+	zombieX = zombieX + speed1;
+	zombie2X = zombie2X + 5;
+	
+	if(zombieX >= edge){
+	zombieX = 0;
+	// speed1 = speed1 + 1;
+	size = size + 20;
+	}
+	if (zombie2X >= edge) {
+	zombie2X = 0;
+	}
+} 
